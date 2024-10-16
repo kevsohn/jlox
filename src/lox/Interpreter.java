bef272c462bd32d2d31dddb832ab27133522b998
Kevin Sohn <kevinsohn@Kevins-MacBook-Air.local>
2024-10-16 23:13:52 +0100

    Interpreter now catches runtime errors

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -2,6 +2,15 @@ package lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
 
+    public void interpret(Expr expr) {
+        try {
+            Object val = evaluate(expr);
+            System.out.println(stringify(val));
+        }catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
@@ -18,21 +27,74 @@ public class Interpreter implements Expr.Visitor<Object> {
         return a.equals(b);
     }
 
+    private String stringify(Object obj) {
+        if (obj == null) return "nil";
+        else if (obj instanceof Double) {
+            String text = obj.toString();
+            if (text.endsWith(".0"))
+                return text.substring(0, text.length()-2);
+        }
+        return obj.toString();
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator,"Operand must be a number.");
+    }
+
+    private void checkNumberOperand(Object left, Token operator, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator,"Operands must be numbers.");
+    }
+
     public Object visitBinary(Expr.Binary expr) {
         Object left = evaluate(expr.left);
         Object right = evaluate(expr.right);
         return switch (expr.op.type) {
-            case PLUS -> (left instanceof Double && right instanceof Double)
-                    ? (Double)left + (Double)right : (String)left + (String)right;
-            case MINUS -> (Double)left - (Double)right;
-            case STAR -> (Double)left * (Double)right;
-            case SLASH -> (Double)left / (Double)right;
-            case MOD -> (Double)left % (Double)right;
-            case HAT-> Math.pow((Double)left, (Double)right);
-            case GREATER -> (Double)left > (Double)right;
-            case GREATER_EQ -> (Double)left >= (Double)right;
-            case LESS -> (Double)left < (Double)right;
-            case LESS_EQ -> (Double)left <= (Double)right;
+            case PLUS -> {
+                if (left instanceof Double && right instanceof Double)
+                    yield (Double)left + (Double)right;
+                else if (left instanceof String && right instanceof String)
+                    yield (String)left + (String)right;
+                else
+                    throw new RuntimeError(expr.op,"Operands must be two numbers or two strings.");
+            }
+            case MINUS -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left - (Double)right;
+            }
+            case STAR -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left * (Double)right;
+            }
+            case SLASH -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left / (Double)right;
+            }
+            case MOD -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left % (Double)right;
+            }
+            case HAT -> {
+                checkNumberOperand(left, expr.op, right);
+                yield Math.pow((Double)left, (Double)right);
+            }
+            case GREATER -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left > (Double)right;
+            }
+            case GREATER_EQ -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left >= (Double)right;
+            }
+            case LESS -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left < (Double)right;
+            }
+            case LESS_EQ -> {
+                checkNumberOperand(left, expr.op, right);
+                yield (Double)left <= (Double)right;
+            }
             case EQ_EQ -> isEqual(left,right);
             case BANG_EQ -> !isEqual(left,right);
             default -> null;
@@ -42,7 +104,10 @@ public class Interpreter implements Expr.Visitor<Object> {
     public Object visitUnary(Expr.Unary expr) {
         Object right = evaluate(expr.right);
         return switch (expr.op.type) {
-            case MINUS -> -(double) right;
+            case MINUS -> {
+                checkNumberOperand(expr.op, right);
+                yield -(double)right;
+            }
             case BANG -> !isTruthy(right);
             default -> null;
         };
--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -9,13 +9,14 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-    // prob want to access this at some point in other class
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
-            System.exit(1);
+            System.exit(65);
         }
         else if (args.length == 1)
             runFile(args[0]);
@@ -27,6 +28,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -44,7 +46,7 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        System.out.print("tokens: ");
+        System.out.print("lexer: ");
         for (Token t: tokens)
             System.out.print(t.lexeme+" ");
         System.out.println();
@@ -60,7 +62,14 @@ public class Lox {
         Parser parser = new Parser(tokens);
         Expr expr = parser.parse();
         if (hadError) return;
-        System.out.println(new AstPrinter().print(expr));
+        System.out.println("parser: "+new AstPrinter().print(expr));
+
+        interpreter.interpret(expr);
+    }
+
+    static void runtimeError(RuntimeError e) {
+        System.err.println(e.getMessage()+"\n[line "+e.token.line+"]");
+        hadRuntimeError = true;
     }
 
     static void error(Token token, String message) {
