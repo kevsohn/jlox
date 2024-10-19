a7e7249168a6fc46eedda142ce615dafcd0bde8e
Kevin Sohn <kevinsohn@Kevins-MacBook-Air.local>
2024-10-19 16:01:28 +0100

    Fixed a bug in Environment.java

--- src/lox/Environment.java
+++ src/lox/Environment.java
@@ -8,18 +8,13 @@ public class Environment {
 
     // return nil if var initializer is not set
     void define(String name, Object value) {
-        //if (value == null)
+        values.put(name, value);
     }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme))
             return values.get(name.lexeme);
         throw new RuntimeError(name,"Undefined variable: '"+name.lexeme+"'.");
-    }
-
-    void put(String name, Object value) {
-        // intentionally override previous binding
-        values.put(name, value);
     }
 
 }
